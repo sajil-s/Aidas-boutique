d0006fc58b6b34d1e64950f3b395762aaaf5607b
Sajil.S <sajilsj19@gmail.com>
2026-07-15 17:32:11 +0530

    Use environment variable for CORS origin

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:3000",
+     origin: process.env.CLIENT_URL,
      credentials: true,
 }))
 app.use("/api/auth",authRoutes);
